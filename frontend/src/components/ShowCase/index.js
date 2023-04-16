1ba2f38d119fd85b92f4a25478bf4e408c3a55b0
jemimah <jemimah@wirebox.co.uk>
2023-04-16 04:54:58 +0100

    more

--- frontend/src/components/ShowCase/index.js
+++ frontend/src/components/ShowCase/index.js
@@ -4,28 +4,7 @@ import { Card, Container, Row, Col } from 'react-bootstrap';
 const ShowCase = () => {
   return (
     <Container>
-      <Card>
-        <Row>
-          <Col md={6}>
-            <Card.Img
-              variant="top"
-              src=""
-              style={{ height: '100%' }}
-              muted
-              autoPlay
-              loop
-            />
-          </Col>
-          <Col md={6}>
-            <Card.Body>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Card.Text>
-              <Card.Title>Video Card Title</Card.Title>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Card>
+      
     </Container>
   );
 };
